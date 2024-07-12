5de81d5130d4d8d3384d9598d45649bfd6228b74
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 10:41:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+escola = "Infinity school" # String str
+quantidadeDeAlunos = 750 # int
+media = 7.5 # Float
+
+print("Olá mundo")
+print(quantidadeDeAlunos)
+
+produto1 = 95.64
+produto2 = 54.63
+totalCompra = produto1 + produto2
+print("Total: R$", totalCompra)
+
+numeroParcelas = 5
+valorParcela = totalCompra / numeroParcelas
+print(f'Valor da parcela: {valorParcela:.2f}') #determina o número de casa decimais
